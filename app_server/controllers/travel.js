f07f8d3128ae8486c925699a5457d8b4048be0c7
mytesting254 <83026481+mytesting254@users.noreply.github.com>
2023-05-31 21:14:43 -0400

    set up HTTP client to call the API endpoints for data access

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,53 @@
-const fs = require('fs');
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
-
-/* Get traveler view */
-const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel'
-    res.render('travel', {title: pageTitle, trips});
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
 }
 
+/* internal method to render the travel list*/
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No trips exist in our database!';
+        }
+    }
+    
+    res. render('travel',
+        {
+            title: pageTitle,
+            trips: responseBody,
+            message
+        }
+    );
+}
+
+/* Get traveler view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> travelContoller.travelList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    );
+};
+
 module.exports = {
-    travel
+    travelList
 }
